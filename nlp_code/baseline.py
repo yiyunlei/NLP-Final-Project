767de18c738e0d89cb039f02254c1a5aab547d43
Yiyun <im.yiyun.lei@gmail.com>
2023-04-19 01:27:20 -0400

    Data preprocess  & Baseline

---
+++ nlp_code/baseline.py
@@ -0,0 +1,95 @@
+# Python Draft for Baseline
+
+import json
+
+# Load simplify data
+
+
+def main():
+    dataset_dir = "./dataset/simplify_json"
+    json_files = [dataset_dir + "/train_split.json", dataset_dir + "/valid_split.json"]
+    data_files = []
+    for json_file in json_files:
+        with open(json_file, 'r') as f:
+            data_files.append(json.load(f))
+
+    # Get (X_train y_train), (X_valid, y_valid) from json file
+    # covert big-5 ['Openness', 'Conscientiousness', 'Extraversion','Agreeableness', 'Neuroticism'] to 0:low 1:high
+    for i in range(len(data_files)):
+        for j in range(len(data_files[i])):
+            data_files[i][j]['label'] = [0 if data_files[i][j]['Openness'] == 'low' else 1,
+                                         0 if data_files[i][j]['Conscientiousness'] == 'low' else 1,
+                                         0 if data_files[i][j]['Extraversion'] == 'low' else 1,
+                                         0 if data_files[i][j]['Agreeableness'] == 'low' else 1,
+                                         0 if data_files[i][j]['Neuroticism'] == 'low' else 1]
+
+
+
+    X_train = []   # name, Utterance
+    y_train = []   # big-5
+    X_valid = []    # name, Utterance
+    y_valid = []    # big-5
+    speakers_train, speakers_valid = [],[]
+    for i in range(len(data_files[0])):
+        X_train.append((data_files[0][i]['Speaker'], data_files[0][i]['Utterance']))
+        y_train.append(data_files[0][i]['label'])
+        speakers_train.append(data_files[0][i]['Speaker'])
+    speakers_train = list(set(speakers_train))
+    print("There are {} speakers in train_data" .format(len(speakers_train)))
+
+    for i in range(len(data_files[1])):
+        X_valid.append((data_files[1][i]['Speaker'], data_files[1][i]['Utterance']))
+        y_valid.append(data_files[1][i]['label'])
+        speakers_valid.append(data_files[1][i]['Speaker'])
+    speakers_valid = list(set(speakers_valid))
+    print("There are {} speakers in valid_data" .format(len(speakers_valid)))
+
+
+def combine_same_speaker_data(speakers, X, y):
+    # combine same speaker data to one data and only keep different Utterance and one speaker name
+    X_train_combine = []
+    y_train_combine = []
+    for speaker in speakers_train:
+        X_train_combine.append({speaker:[]})
+        y_train_combine.append({speaker:[]})
+
+
+
+    for i in range(len(X_train)):
+        for j in range(len(X_train_combine)):
+            if X_train[i][0] in X_train_combine[j]:
+                X_train_combine[j][X_train[i][0]].append(X_train[i][1])
+                y_train_combine[j][X_train[i][0]].append(y_train[i])
+
+
+    for i in range(len(y_train_combine)):
+        for key in y_train_combine[i]:
+            y_train_combine[i][key] = y_train_combine[i][key][0]
+    return X_train_combine, y_train_combine
+
+X_train_combined, y_train_combined = combine_same_speaker_data(speakers_train, X_train, y_train)
+X_valid_combined, y_valid_combined = combine_same_speaker_data(speakers_valid, X_valid, y_valid)
+
+
+# encoding=utf-8
+
+seg_list = jieba.cut("我来到北京清华大学", cut_all=False)
+print("Default Mode: " + " ".join(seg_list))  # Accruate Mode
+# use jieba to cut sentence on x_valid_combined
+
+for speaker in x_valid_combined:
+    for key in speaker:
+        for i in range(len(speaker[key])):
+            seg_list = jieba.cut(speaker[key][i], cut_all=False)
+            speaker[key][i] = " ".join(seg_list)
+
+
+
+# encoding=utf-8
+
+
+
+
+if __name__ == "__main__":
+    main()
+              
---
+++ nlp_code/data_preprocess.py
@@ -0,0 +1,141 @@
+# basic code are from assignment2, improve and update by Yiyun Lei
+import os
+import sys
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+import sklearn.metrics as metrics
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+import gzip
+import csv, re
+import string
+from tqdm import tqdm
+import codecs
+import argparse
+from collections import Counter
+from spacy.lang.en import English
+ 
+
+# Convenient for debugging but feel free to comment out
+
+# Hard-wired variables
+input_speechfile   = "./speeches2020_jan_to_jun.jsonl.gz"
+stopwords_file     = "./mallet_en_stoplist.txt"
+
+train_csv_file     = "./dataset/CPED/train_split.csv"
+valid_csv_file     = "./dataset/CPED/valid_split.csv"
+test_csv_file      = "./dataset/CPED/test_split.csv"
+csv_files         = [train_csv_file, valid_csv_file, test_csv_file]
+
+train_json_file    = "./dataset/json/train_split.json"
+valid_json_file    = "./dataset/json/valid_split.json"
+test_json_file     = "./dataset/json/test_split.json"
+json_files        = [train_json_file, valid_json_file, test_json_file]
+
+train_simplify_json_file    = "./dataset/simplify_json/train_split.json"
+valid_simplify_json_file    = "./dataset/simplify_json/valid_split.json"
+test_simplify_json_file     = "./dataset/simplify_json/test_split.json"
+simplify_json_files        = [train_simplify_json_file, valid_simplify_json_file, test_simplify_json_file]
+
+
+
+# Covert csv file to json file
+def csv_to_json(csv_file, json_file):
+    with open(csv_file, 'r') as f:
+        reader = csv.DictReader(f)
+        rows = list(reader)
+    with open(json_file, 'w', encoding='utf-8') as f:
+        json.dump(rows, f, ensure_ascii = False)
+    print("Convert successfully".format(csv_file, json_file))
+
+
+# Simplify and clean data
+def simplify_data(json_file, simplify_json_file):
+    # Use order OCEAN 
+    keys = ['Openness', 'Conscientiousness', 'Extraversion','Agreeableness', 'Neuroticism', 'Speaker', 'Utterance']
+    new_data = []
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+    for obj in data:
+        new_data.append({key: obj[key] for key in keys})
+    with open(simplify_json_file, 'w', encoding='utf-8') as f:
+        json.dump(new_data, f, ensure_ascii = False)
+
+# Clean data and one hot encoding 
+# low - 0
+# high - 1
+def clean_data(json_file):
+    count = 0
+    keys = ['Openness', 'Conscientiousness', 'Extraversion','Agreeableness', 'Neuroticism', 'Speaker', 'Utterance']
+    # print rows that have Unknown value
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+    updated_data = []
+    for obj in data:
+        if obj['Speaker'] != '其他' and all(obj[key] != 'unknown' for key in keys):
+            updated_data.append(obj)
+            count += 1
+    print("count:", count)
+    with open(json_file, 'w', encoding='utf-8') as f:
+        json.dump(updated_data, f, ensure_ascii = False)
+    
+
+# Get (X_train y_train), (X_valid, y_valid, X_test, y_test) from json file
+def get_X_y(json_file):
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+    X = []
+    y = []
+    for i in range(len(data)):
+        X.append(data[i]['name, Utterance'])
+        y.append(data[i]['label'])
+    return X, y
+
+def validate_unique_big5_of_speaker(simplify_json_file):
+    with open(simplify_json_file, 'r') as f:
+        data = json.load(f)
+    speakers = []
+    for obj in data:
+        speakers.append(obj['Speaker'])
+    speakers = list(set(speakers))
+    print("There are {} speakers in {}" .format(len(speakers), simplify_json_file))
+    for speaker in speakers:
+        data_of_same_speaker = []
+        for obj in data:
+            if obj['Speaker'] == speaker:
+                data_of_same_speaker.append(obj)
+        # print("Speaker {} has {} data" .format(speaker, len(data_of_same_speaker)))
+        if not validate_unique_big5(data_of_same_speaker):
+            print("Speaker {} has different big5 in {}" .format(speaker, simplify_json_file))
+            return False
+    return True
+
+def validate_unique_big5(data_of_same_speaker):
+    # print("datase:", data_of_same_speaker[0])
+    """Validate that the big5 for each speaker is unique"""
+    keys = ['Openness', 'Conscientiousness', 'Extraversion', 'Agreeableness', 'Neuroticism']
+    big5 = {key: data_of_same_speaker[0][key] for key in keys}
+    for data in data_of_same_speaker[1:]:
+        current_big5 = {key: data[key] for key in keys}
+        if current_big5 != big5:
+            return False
+    return True
+
+def main():
+    # preprocess data
+    for csv_file, json_file, simplify_json_file in zip(csv_files, json_files, simplify_json_files):
+        # print('current covert {} file to {}:' .format(csv_file, json_file))
+        # csv_to_json(csv_file,json_file)
+        # simplify_data(json_file, simplify_json_file)
+        # clean_data(simplify_json_file)
+        # validation unique big5 for each speaker
+        if not validate_unique_big5_of_speaker(simplify_json_file):
+            print("There are different big5 for same speaker in {}" .format(simplify_json_file))
+        print("There are no different big5 for same speaker in {}" .format(simplify_json_file))
+
+
+if __name__ == "__main__":
+    main()
+    
